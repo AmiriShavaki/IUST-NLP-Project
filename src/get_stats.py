eb369239523da1ea43c77f281636c78bd05ee8ea
Mahdi Amiri <mahdi_amiri98@comp.iust.ac.ir>
2023-06-01 09:25:44 +0330

    +Word Counts

--- src/get_stats.py
+++ src/get_stats.py
@@ -51,3 +51,28 @@ output_file2.write('\n'.join((
 output_file.close()
 output_file2.close()
 
+
+## Number of words
+
+# Plot
+dfs = [pd.read_csv(f'../data/clean/{i}star.csv') for i in range(1, 5+1)]
+word_cnt = [sum(dfs[i]['0'].apply(lambda x:len(x.split()))) for i in range(5)]
+labels = tuple(f'{i} star' for i in range(1, 5+1))
+fig, ax = plt.subplots()
+ax.pie(sen_cnt, labels=labels, autopct='%1.1f%%', pctdistance=1.25, labeldistance=.6)
+fig.savefig("../stats/word_cnt.png")
+fig.savefig("../latex_report/Images/word_cnt.png")
+
+# Table
+output_file = open("../latex_report/tables/word_cnt.csv", "w", encoding="utf-8")
+output_file2 = open("../stats/word_cnt.csv", "w", encoding="utf-8")
+output_file.write('\n'.join((
+    ",۱ ستاره,۲ ستاره,۳ ستاره, ۴ ستاره, ۵ ستاره, همه‌ برچسب‌ها",
+    "تعداد کلمات, {},{},{},{},{},{}".format(*word_cnt, sum(word_cnt))
+)))
+output_file2.write('\n'.join((
+    ",۱ ستاره,۲ ستاره,۳ ستاره, ۴ ستاره, ۵ ستاره, همه‌ برچسب‌ها",
+    "تعداد کلمات, {},{},{},{},{},{}".format(*word_cnt, sum(word_cnt))
+)))
+output_file.close()
+output_file2.close()
