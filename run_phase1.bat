c3707537b70214cd34e1015ea5ebd8fa9197f9fa
Mahdi Amiri <mahdi_amiri98@comp.iust.ac.ir>
2023-07-07 22:27:08 +0330

    Add comment to the batch script

--- run_phase1.bat
+++ run_phase1.bat
@@ -1,3 +1,6 @@
+:: You can uncomment two below lines and replace your desired virtual environment
+:: call C:/ProgramData/Anaconda3/Scripts/activate
+:: call conda activate cs224n
 python -m src.collect_raw_data
 python -m src.clean_collected_raw_data
 python -m src.wordbreak_and_sentencebreak_cleaned_data
