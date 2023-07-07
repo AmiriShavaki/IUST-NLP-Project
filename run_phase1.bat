91450f7ba9a3a3a7afac107d17b1755cffc53f46
Mahdi Amiri <mahdi_amiri98@comp.iust.ac.ir>
2023-07-07 21:49:36 +0330

    Fix run script

--- run_phase1.bat
+++ run_phase1.bat
@@ -1,5 +1,5 @@
-python -m src.collect_raw_data.py
-python -m src.clean_collected_raw_data.py
-python -m src.wordbreak_and_sentencebreak_cleaned_data.py
-python -m src.total_data_size_before_after_cleaning.py
+python -m src.collect_raw_data
+python -m src.clean_collected_raw_data
+python -m src.wordbreak_and_sentencebreak_cleaned_data
+python -m src.total_data_size_before_after_cleaning
 PAUSE
